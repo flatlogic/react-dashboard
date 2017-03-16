3caca2d8808ab331d15b8006aac132169ed606d1
okendoken <philip@flatlogic.com>
2017-03-16 17:05:44 +0300

    feat: remove universal-router routes

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -11,21 +11,13 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Contact.css';
 
-class Contact extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>{this.props.title}</h1>
-          <p>...</p>
-        </div>
-      </div>
-    );
-  }
-}
+const Contact = () => (
+  <div className={s.root}>
+    <div className={s.container}>
+      <h1>Contact</h1>
+      <p>...</p>
+    </div>
+  </div>
+);
 
 export default withStyles(s)(Contact);
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -12,20 +12,12 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 
 class Home extends React.Component {
-  static propTypes = {
-    news: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-      content: PropTypes.string,
-    })).isRequired,
-  };
 
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>React.js News</h1>
-
         </div>
       </div>
     );
